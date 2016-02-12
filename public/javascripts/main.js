a716300c88eb901016e37dca0e1859a081aafad3
Metel Patel <metelpatel@gmail.com>
2016-02-11 22:08:26 -0600

    key is correct

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -34,7 +34,7 @@ $(function(){
     $('#beerPic').empty();
 
     var jqxhr = $.ajax({
-      url: '/api/beer/random?key=c2edb5cd55db30ff7a0f795ac6bff1ea9',
+      url: '/api/beer/random?key=c2edb5cd55db30ff7a0f795ac6bff1ea',
       method: "GET",
       data: {},
       dataType: "json"
