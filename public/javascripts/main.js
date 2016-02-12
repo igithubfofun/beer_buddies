b73495aae5717d6180e6e4a5c9c81f1537d1207e
Metel Patel <metelpatel@gmail.com>
2016-02-12 08:48:16 -0600

    second key

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -32,9 +32,9 @@ $(function(){
     $('#beerName').empty();
     $('#beerDesc').empty();
     $('#beerPic').empty();
-
+//c2edb5cd55db30ff7a0f795ac6bff1ea9
     var jqxhr = $.ajax({
-      url: '/api/beer/random?key=c2edb5cd55db30ff7a0f795ac6bff1ea',
+      url: '/api/beer/random?key=21284e4a1b4e205accad9dfc8afff0b8',
       method: "GET",
       data: {},
       dataType: "json"
