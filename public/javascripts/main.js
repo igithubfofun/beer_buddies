f25640e8c1685b6d05a3ad8a97bfead3e0782853
Metel Patel <metelpatel@gmail.com>
2016-02-11 19:20:32 -0600

    all day

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -119,7 +119,7 @@ $('#submitBeer').on('submit', function() {
     $('#liquid') // I Said Fill 'Er Up!
       .delay(3400)
       .animate({
-        height: '100px'
+        height: '200px'
       }, 2500);
 
     $('.beer-foam') // Keep that Foam Rollin' Toward the Top! Yahooo!
